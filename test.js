11d2df87f7fc985a0b5a0bc902d448a5b05a1760
Jimmy Wärting <jimmy@warting.se>
2021-08-07 13:08:44 +0200

    converted to esm

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
-'use strict'
-
-var names = require('./');
-var assert = require('assert');
+import assert from 'assert';
+import names from './index.js';
 
 assert.deepEqual(names.red, [255,0,0]);
 assert.deepEqual(names.aliceblue, [240,248,255]);
